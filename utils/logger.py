ac7ea743ab06eb1c7a20b34fb025770c4cdf2227
bruteai <57473568+bruteai@users.noreply.github.com>
2025-04-05 21:50:51 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,17 @@
-# logger.py - placeholder content
+# utils/logger.py
+import logging
+
+def get_logger(name=__name__):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+    return logger
+
+if __name__ == "__main__":
+    log = get_logger("TestLogger")
+    log.info("Logger initialized.")
